0d9b259000109bf0a9da5280f9ab80d6aebae0dc
Justin Ng <justinng123409@yahoo.com>
2021-09-17 00:18:36 -0700

    problem 362

---
+++ Array/Problem362.js
@@ -0,0 +1,95 @@
+/*
+362. Design Hit Counter
+Medium
+
+Design a hit counter which counts the number of hits received in the past 5 minutes (i.e., the past 300 seconds).
+
+Your system should accept a timestamp parameter (in seconds granularity), and you may assume that calls are being made to the system in chronological order (i.e., timestamp is monotonically increasing). Several hits may arrive roughly at the same time.
+
+Implement the HitCounter class:
+
+HitCounter() Initializes the object of the hit counter system.
+void hit(int timestamp) Records a hit that happened at timestamp (in seconds). Several hits may happen at the same timestamp.
+int getHits(int timestamp) Returns the number of hits in the past 5 minutes from timestamp (i.e., the past 300 seconds).
+ 
+
+Example 1:
+
+Input
+["HitCounter", "hit", "hit", "hit", "getHits", "hit", "getHits", "getHits"]
+[[], [1], [2], [3], [4], [300], [300], [301]]
+Output
+[null, null, null, null, 3, null, 4, 3]
+
+Explanation
+HitCounter hitCounter = new HitCounter();
+hitCounter.hit(1);       // hit at timestamp 1.
+hitCounter.hit(2);       // hit at timestamp 2.
+hitCounter.hit(3);       // hit at timestamp 3.
+hitCounter.getHits(4);   // get hits at timestamp 4, return 3.
+hitCounter.hit(300);     // hit at timestamp 300.
+hitCounter.getHits(300); // get hits at timestamp 300, return 4.
+hitCounter.getHits(301); // get hits at timestamp 301, return 3.
+ 
+
+Constraints:
+
+1 <= timestamp <= 2 * 109
+All the calls are being made to the system in chronological order (i.e., timestamp is monotonically increasing).
+At most 300 calls will be made to hit and getHits.
+*/
+
+/**
+ * Initialize your data structure here.
+ */
+ var HitCounter = function() {
+    // this.hits = 0;
+    this.timestamps = new Array();
+    
+};
+
+/**
+ * Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). 
+ * @param {number} timestamp
+ * @return {void}
+ */
+HitCounter.prototype.hit = function(timestamp) {
+    // console.log(this.timestamps[0])
+//     while(timestamp>300 && timestamp-300>=this.timestamps[0]){
+        
+//         // console.log(1, this.timestamps)
+//         this.timestamps.shift();
+//         // console.log(2, this.timestamps)
+//     }
+
+    this.timestamps.push(timestamp)
+    // console.log(this.timestamps)
+    
+    
+};
+
+/**
+ * Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). 
+ * @param {number} timestamp
+ * @return {number}
+ */
+HitCounter.prototype.getHits = function(timestamp) {
+    while(timestamp>300 && timestamp-300>=this.timestamps[0]){
+        
+        // console.log(1, this.timestamps)
+        this.timestamps.shift();
+
+        // console.log(2, this.timestamps)
+    }
+    
+    return this.timestamps.length;
+};
+
+/** 
+ * Your HitCounter object will be instantiated and called as such:
+ * var obj = new HitCounter()
+ * obj.hit(timestamp)
+ * var param_2 = obj.getHits(timestamp)
+ */
