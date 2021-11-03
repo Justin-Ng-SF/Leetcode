1efab37ac4c5ec2d9facb466c40a4dd31abfd177
Justin Ng <justinng123409@yahoo.com>
2021-11-02 23:07:12 -0700

    rotate arr

---
+++ Array/rotateArr.js
@@ -0,0 +1,56 @@
+/*
+189. Rotate Array
+Medium
+
+6481
+
+1081
+
+Add to List
+
+Share
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+ */
+var rotate = function (nums, k) {
+    k %= nums.length // if k is greater than nums.length then one cycle is completed that means it will remain the same and we have to remainder shifts
+     
+    let reverse = function(i, j){
+     while(i < j){
+         let temp = nums[i]
+         nums[i] = nums[j]
+         nums[j] = temp
+         i++
+         j--
+     }
+    } // suppose  ----->---> 
+     reverse(0, nums.length-1); // reverse   <--<------
+      reverse(0, k-1) // reverse first part ---><----
+    reverse(k, nums.length-1)// reverse second part --->----->
+     
+ };
+ 
